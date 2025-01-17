c831a406a14daa70ee36620ee3397303006fabaf
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-01-17 18:37:04 +0100

    Initial commit: Base setup for HirePortal2025 application

---
+++ src/main/java/com/HirePortal2025/HirePortal2025/HirePortal2025Application.java
@@ -0,0 +1,13 @@
+package com.HirePortal2025.HirePortal2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirePortal2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirePortal2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HirePortal2025/HirePortal2025/HirePortal2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HirePortal2025.HirePortal2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirePortal2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
