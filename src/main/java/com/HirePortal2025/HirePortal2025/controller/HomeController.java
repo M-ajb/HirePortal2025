7b7bd4029d030b148bddfb439ca07e6d110c0e67
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-01-22 01:18:35 +0100

    Added templates for registration functionality

---
+++ src/main/java/com/HirePortal2025/HirePortal2025/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.HirePortal2025.HirePortal2025.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
