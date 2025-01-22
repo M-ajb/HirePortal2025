72602c47e96f0596c74fa40699f49e3c8725ea61
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-01-22 17:14:04 +0100

    Resolve issue: prevent duplicate email registrations and implement error message for duplicates

--- src/main/java/com/HirePortal2025/HirePortal2025/repository/UsersRepository.java
+++ src/main/java/com/HirePortal2025/HirePortal2025/repository/UsersRepository.java
@@ -3,5 +3,9 @@ package com.HirePortal2025.HirePortal2025.repository;
 import com.HirePortal2025.HirePortal2025.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByEmail(String email);
 }
