387ce67e91642a798d3548df4289ebe9f95ff622
M-ajb <195341850+M-ajb@users.noreply.github.com>
2025-01-30 17:24:34 +0100

    Implement job retrieval and display on Recruiter Dashboard

--- src/main/java/com/HirePortal2025/HirePortal2025/controller/JobPostActivityController.java
+++ src/main/java/com/HirePortal2025/HirePortal2025/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.HirePortal2025.HirePortal2025.controller;
 
 import com.HirePortal2025.HirePortal2025.entity.JobPostActivity;
+import com.HirePortal2025.HirePortal2025.entity.RecruiterJobsDto;
+import com.HirePortal2025.HirePortal2025.entity.RecruiterProfile;
 import com.HirePortal2025.HirePortal2025.entity.Users;
 import com.HirePortal2025.HirePortal2025.services.JobPostActivityService;
 import com.HirePortal2025.HirePortal2025.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -39,6 +43,11 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+               List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile)currentUserProfile).getUserAccountId());
+               model.addAttribute("jobPost", recruiterJobs);
+
+            }
         }
         model.addAttribute("user" , currentUserProfile);
 
---
+++ src/main/java/com/HirePortal2025/HirePortal2025/entity/IRecruiterJobs.java
@@ -0,0 +1,22 @@
+package com.HirePortal2025.HirePortal2025.entity;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+
+    int getJob_Post_Id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+
+    String getState();
+
+    String getCountry();
+
+    int getCompanyId();
+
+    String getName();
+}
